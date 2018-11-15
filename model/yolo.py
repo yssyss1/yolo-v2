17850b5c09792d6342f8b1865f75219c88e509f9
seok <seok11754@gmail.com>
2018-11-15 15:01:39 +0900

    modify model/yolo.py : naive yolo v2 - change loss function wrt yolo v1 paper

--- model/yolo.py
+++ model/yolo.py
@@ -154,7 +154,7 @@ class YOLO:
         nb_class_box = tf.reduce_sum(tf.to_float(class_mask > 0.0))
 
         loss_xy = tf.reduce_sum(tf.square(true_box_xy - pred_box_xy) * coord_mask) / (nb_coord_box + 1e-6) / 2.
-        loss_wh = tf.reduce_sum(tf.square(true_box_wh - pred_box_wh) * coord_mask) / (nb_coord_box + 1e-6) / 2.
+        loss_wh = tf.reduce_sum(tf.square(tf.sqrt(true_box_wh) - tf.sqrt(pred_box_wh)) * coord_mask) / (nb_coord_box + 1e-6) / 2.
         loss_conf = tf.reduce_sum(tf.square(true_box_conf - pred_box_conf) * conf_mask) / (nb_conf_box + 1e-6) / 2.
         loss_class = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class, logits=pred_box_class)
         loss_class = tf.reduce_sum(loss_class * class_mask) / (nb_class_box + 1e-6)
